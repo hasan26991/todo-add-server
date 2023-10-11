3763ff3bfbc58c44819e24e4acb2000c1cc5cdce
Hassan Fawaz <h.fawaz@ids.com.lb>
2023-10-11 19:52:17 +0300

    test 2

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,8 +1,8 @@
 const allowedOrigins = [
   "http://127.0.0.1:3000",
   "http://localhost:3000",
-  "https://main--unrivaled-llama-b97cbb.netlify.app",
   "https://app-todo-next.netlify.app",
+  "https://main--app-todo-next.netlify.app",
 ];
 
 export { allowedOrigins };
